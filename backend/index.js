abc0dc27a9aa389642746f929abeea9632fe32d6
raoufbouk <zakib2316@gmail.com>
2024-12-04 19:28:19 +0100

    Create our first Http Route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { PORT } from "./config";
+import { PORT } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(200).send("Hello World");
+});
+
+app.listen(5555, () => {
   console.log(`Server is running on port ${PORT}`);
 });
