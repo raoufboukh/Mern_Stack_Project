fa7b7cfb6c85b82d7dd49b5de30db21fd5fb10f3
raoufbouk <zakib2316@gmail.com>
2024-12-06 14:22:40 +0100

    Make Book Card a single component

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,43 +1,12 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BiUserCircle } from "react-icons/bi";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineDelete } from "react-icons/md";
-import { PiBookOpenTextLight } from "react-icons/pi";
+
+import BookSingleCard from "./BookSingleCard";
 
 const BooksCard = ({ books }) => {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
       {books.map((book) => (
-        <div
-          key={books._id}
-          className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl transition-all"
-        >
-          <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
-            {book.publishYear}
-          </h2>
-          <h4 className="my-2 text-gray-500">{book._id}</h4>
-          <div className="flex justify-start items-center gap-x-2">
-            <PiBookOpenTextLight className="text-2xl text-red-300" />
-            <h2 className="my-1">{book.title}</h2>
-          </div>
-          <div className="flex justify-start items-center gap-x-2">
-            <BiUserCircle className="text-2xl text-red-300" />
-            <h2 className="my-1">{book.author}</h2>
-          </div>
-          <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
-            <Link to={`/books/details/${book._id}`}>
-              <BsInfoCircle className="text-2xl text-green-800" />
-            </Link>
-            <Link to={`/books/edit/${book._id}`}>
-              <AiOutlineEdit className="text-2xl text-yellow-600" />
-            </Link>
-            <Link to={`/books/delete/${book._id}`}>
-              <MdOutlineDelete className="text-2xl text-red-800" />
-            </Link>
-          </div>
-        </div>
+        <BookSingleCard key={book._id} book={book} />
       ))}
     </div>
   );
